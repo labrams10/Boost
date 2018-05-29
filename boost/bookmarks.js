5ae5975b132b91ad4ea0b6710cb0332a21cbdf37
patmccler <patmccler@gmail.com>
2018-05-29 16:21:24 -0400

    proof of concept: add href from breadcrumb to storag and retrieve it

--- boost/bookmarks.js
+++ boost/bookmarks.js
@@ -1,6 +1,25 @@
 console.log("bookmarks.js");
 
 let nav = document.querySelector("ol.nav");
+let home = document.getElementById("navHome");
+let second = home.nextElementSibling;
 
-console.log(nav);
-console.log(nav.firstChild);
+console.log(home);
+
+console.log(second);
+console.log(second.firstElementChild.href);
+
+chrome.storage.local.set({
+  testBookmarkStorage: [
+    second.firstElementChild.href,
+    second.nextElementSibling.firstChild.href
+  ]
+});
+
+setTimeout(getFromStorage, 3000);
+
+function getFromStorage() {
+  chrome.storage.local.get("testBookmarkStorage", function(result) {
+    console.log(result.testBookmarkStorage);
+  });
+}
