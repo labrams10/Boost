36ed342c9e2a43528267c6217d9ed0ddc1950d15
patmccler <patmccler@gmail.com>
2018-06-28 18:44:06 -0400

    saving last changes

--- boost/bookmarks.js
+++ boost/bookmarks.js
@@ -23,3 +23,6 @@ function getFromStorage() {
     console.log(result.testBookmarkStorage);
   });
 }
+
+//INFO - Element.pathname returns the "/support/tickets" bit that is actually listed as the href when inspecting the element
+// Assuming that <a> elements list the pathname and build the full link if just given a relative path
